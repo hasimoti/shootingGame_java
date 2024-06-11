575d06920e9bac9281ca67e09e5e149ae3609386
soutahasimoto <hasimoto.s511@gmail.com>
2024-06-11 12:19:37 +0900

    コミット

--- shooting/src/GameMain.java
+++ shooting/src/GameMain.java
@@ -1,9 +1,11 @@
 
-public class GameMain {
+public class GameMain 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO 自動生成されたメソッド・スタブ
-
+		new GameFrame();
 	}
 
 }
