b97546d7af19bbbd263add62bcb8c3104229c9fd
soutahasimoto <hasimoto.s511@gmail.com>
2024-06-18 10:04:57 +0900

    11.2敵を動かす

--- shooting/src/GameFrame.java
+++ shooting/src/GameFrame.java
@@ -7,12 +7,16 @@ public class GameFrame extends MyFrame
 		GameWorld.player=new Player(100,300,0,0);
 		addKeyListener(GameWorld.player);
 		GameWorld.playerBullets=new Vector<PlayerBullet>();
+		GameWorld.enemies=new Vector<Enemy>();
+		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
 		while(true) 
 		{
 			clear();
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
 			movePlayerBullets();
+			
+			moveEnemmies();
 			sleep(0.01);
 		}
 		
@@ -34,6 +38,16 @@ public class GameFrame extends MyFrame
 				i++;
 			}
 			    
+		}
+	}
+	
+	public void moveEnemmies() 
+	{
+		for(int i=0;i<GameWorld.enemies.size();i++) 
+		{
+			Enemy e=GameWorld.enemies.get(i);
+			e.draw(this);
+			e.move();
 		}
 	}
 }
